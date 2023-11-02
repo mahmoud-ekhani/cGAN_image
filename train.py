a5246f3c66042aa3069a1d3d415594a71604e4eb
mahmoud-ekhani <m.ebrahimkhani1993@gmail.com>
2023-11-02 13:24:48 -0400

    update loading checkpoints

--- train.py
+++ train.py
@@ -1,17 +1,25 @@
+import argparse
+import os
+import glob
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import argparse
-import os
 from torchvision.utils import save_image
+
 from models.generator import Generator
 from models.discriminator import Discriminator
 from data.loader import get_dataloader
 
 # Training the models
 def train(generator, discriminator, criterion, optimizer_G, scheduler_G, optimizer_D, scheduler_D, 
-          dataloader, device, epochs, latent_dim, start_epoch):
+          dataloader, val_dataloader, device, epochs, latent_dim, start_epoch, patience):
+    
+    min_val_loss = float('inf')
+    patience_counter = 0
+
     for epoch in range(start_epoch, start_epoch+epochs):
+        # Train loop
         for i, (imgs, labels) in enumerate(dataloader):
             real = torch.ones(imgs.size(0), 1).to(device)
             fake = torch.zeros(imgs.size(0), 1).to(device)
@@ -43,6 +51,46 @@ def train(generator, discriminator, criterion, optimizer_G, scheduler_G, optimiz
             
             print(f"[Epoch {epoch}/{epochs}] [Batch {i}/{len(dataloader)}] [D loss: {d_loss.item()}] [G loss: {g_loss.item()}]")
 
+        # Validation loop
+        with torch.no_grad():
+            val_g_losses = []
+            val_d_losses = []
+            for i, (imgs, labels) in enumerate(val_dataloader):
+                real = torch.ones(imgs.size(0), 1).to(device)
+                fake = torch.zeros(imgs.size(0), 1).to(device)
+                
+                noise = torch.randn(imgs.size(0), latent_dim).to(device)
+                gen_imgs = generator(noise, labels)  # Use actual labels
+
+                validity = discriminator(gen_imgs, labels)
+                g_loss = criterion(validity, real)
+                val_g_losses.append(g_loss.item())
+
+                real_validity = discriminator(imgs, labels)
+                d_real_loss = criterion(real_validity, real)
+
+                fake_validity = discriminator(gen_imgs.detach(), labels)
+                d_fake_loss = criterion(fake_validity, fake)
+
+                d_loss = (d_real_loss + d_fake_loss) / 2
+                val_d_losses.append(d_loss.item())
+            
+            avg_val_g_loss = sum(val_g_losses) / len(val_g_losses)
+            avg_val_d_loss = sum(val_d_losses) / len(val_d_losses)
+            print(f"Epoch {epoch}/{epochs} - Generator Val Loss: {avg_val_g_loss}, Discriminator Val Loss: {avg_val_d_loss}")
+
+        # Early stopping
+        combined_val_loss = (avg_val_g_loss + avg_val_d_loss) / 2
+        if combined_val_loss < min_val_loss:
+            min_val_loss = combined_val_loss
+            patience_counter = 0
+        else:
+            patience_counter += 1
+
+        if patience_counter >= patience:
+            print(f"Early stopping triggered after {patience} epochs without improvement.")
+            break
+
         # Step the schedulers to adjust the learning rates
         scheduler_G.step()
         scheduler_D.step()
@@ -50,23 +98,22 @@ def train(generator, discriminator, criterion, optimizer_G, scheduler_G, optimiz
         # Save generated samples for visualization
         save_image(gen_imgs.data, f"outputs/images/{epoch}.png", nrow=10, normalize=True)
         
-        if epoch % 5 == 0:
         # Save model checkpoints
-            checkpoint = {
-                'epoch': epoch,
-                'generator_state_dict': generator.state_dict(),
-                'discriminator_state_dict': discriminator.state_dict(),
-                'optimizer_G_state_dict': optimizer_G.state_dict(),
-                'optimizer_D_state_dict': optimizer_D.state_dict(),
-                'scheduler_G_state_dict': scheduler_G.state_dict(),
-                'scheduler_D_state_dict': scheduler_D.state_dict(),
-                }
-            torch.save(checkpoint, f"checkpoint/cgan_checkpoint_{epoch}.pth")
+        checkpoint = {
+            'epoch': epoch,
+            'generator_state_dict': generator.state_dict(),
+            'discriminator_state_dict': discriminator.state_dict(),
+            'optimizer_G_state_dict': optimizer_G.state_dict(),
+            'optimizer_D_state_dict': optimizer_D.state_dict(),
+            'scheduler_G_state_dict': scheduler_G.state_dict(),
+            'scheduler_D_state_dict': scheduler_D.state_dict(),
+            }
+        torch.save(checkpoint, f"checkpoint/cgan_checkpoint_{epoch}.pth")
 
 def main(args):
     # Setup device, models, optimizers, and loss
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    dataloader = get_dataloader(batch_size=args.batch_size)
+    train_dataloader, val_dataloader = get_dataloader(batch_size=args.batch_size, train_size=args.train_size, seed=1910)
 
     generator = Generator(img_shape=args.img_shape[-1], latent_dim=args.latent_dim, n_classes=args.n_classes).to(device)
     discriminator = Discriminator(img_shape=args.img_shape[-1], n_classes=args.n_classes).to(device)
@@ -79,12 +126,13 @@ def main(args):
 
     criterion = nn.BCELoss()
 
-    # Check if checkpoint exists and load saved states
-    checkpoint_path = f"checkpoints/cgan_checkpoint_last.pth"
+    # Find the latest checkpoint in the 'checkpoints' folder
+    checkpoint_files = sorted(glob.glob("checkpoint/cgan_checkpoint_*.path", key=os.path.getmtime)
     start_epoch = 0
 
-    if os.path.exists(checkpoint_path):
-        print(f"Loading checkpoint from {checkpoint_path}...")
+    if checkpoint_files:
+        lates_checkpoint = checkpoint_files[-1]
+        print(f"Loading checkpoint from {latest_checkpoint}...")
         checkpoint = torch.load(checkpoint_path)
         generator.load_state_dict(checkpoint['generator_state_dict'])
         discriminator.load_state_dict(checkpoint['discriminator_state_dict'])
@@ -93,10 +141,11 @@ def main(args):
         scheduler_G.load_state_dict(checkpoint['scheduler_G_state_dict'])
         scheduler_D.load_state_dict(checkpoint['scheduler_D_state_dict'])
         start_epoch = checkpoint['epoch'] + 1
+        print(f"Resuming training from epoch {start_epoch}")
 
     # Train the models
     train(generator, discriminator, criterion, optimizer_G, scheduler_G, optimizer_D, scheduler_D, 
-          dataloader, device, args.epochs, args.latent_dim, start_epoch=start_epoch)
+          train_dataloader, val_dataloader, device, args.epochs, args.latent_dim, start_epoch=start_epoch, patience=args.patience)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -106,6 +155,8 @@ if __name__ == "__main__":
     parser.add_argument("--img_shape", type=tuple, default=(1, 64, 64), help="size of image dimensions")
     parser.add_argument("--n_classes", type=int, default=10, help="number of classes for dataset")
     parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
+    parser.add_argument("--train_size", type=float, default=0.8, help="percentage of training data, the rest will be used for evaluation")
+    parser.add_argument("--patience", type=int, default=20, help="early stopping patience")
     args = parser.parse_args()
 
     main(args)
